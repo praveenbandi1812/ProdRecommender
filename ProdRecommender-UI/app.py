b556ddc8b41e3f7628d637369a5aae0fdadd5770
praveenbandi1812 <86944280+praveenbandi1812@users.noreply.github.com>
2021-09-11 01:54:55 +0800

    11Sep20210154AM

--- ProdRecommender-UI/app.py
+++ ProdRecommender-UI/app.py
@@ -14,12 +14,15 @@ app = Flask(__name__)
 #iris_model = joblib.load('model/iris_model.pkl')
 xgb_model_pk = joblib.load('xgb_model.pkl')
 item_final_rating_pk = joblib.load('item_final_rating.pkl')
-Product_Reviews = pd.read_csv("sample30.csv")
-Item_Reviews=Product_Reviews
-Item_Reviews.drop_duplicates(subset =["reviews_username","name","id"],keep = False, inplace = True)
+Product_Reviews = joblib.load('Product_Reviews_CleanData.pkl')
+word_vectorizer = joblib.load('word_vectorizer.pkl')
+Item_Reviews = joblib.load('Item_Reviews.pkl')
+#Product_Reviews = pd.read_csv("sample30.csv")
+#Item_Reviews=Product_Reviews
+#Item_Reviews.drop_duplicates(subset =["reviews_username","name","id"],keep = False, inplace = True)
     # define vectorize and fit to data     
-word_vectorizer = TfidfVectorizer(sublinear_tf=True,strip_accents='unicode',analyzer='word',token_pattern=r'\w{1,}',stop_words='english',ngram_range=(1, 1),max_features=10000)
-word_vectorizer.fit(Product_Reviews['reviews_text'])
+#word_vectorizer = TfidfVectorizer(sublinear_tf=True,strip_accents='unicode',analyzer='word',token_pattern=r'\w{1,}',stop_words='english',ngram_range=(1, 1),max_features=10000)
+#word_vectorizer.fit(Product_Reviews['reviews_text'])
 
 @app.route('/')
 def home():
@@ -36,19 +39,20 @@ def predict():
     user_input = request.args['username']
     
     #print(sepal_length)
-    Top20_Recomm_products=item_final_rating_pk.loc[user_input].sort_values(ascending=False)[0:20]
-    Top20_Recomm_products_df = pd.DataFrame(Top20_Recomm_products)
+    #Top20_Recomm_products=item_final_rating_pk.loc[user_input].sort_values(ascending=False)[0:20]
+    Top20_Recomm_products_df = pd.DataFrame(item_final_rating_pk.loc[user_input].sort_values(ascending=False)[0:20])
     ##### merging with main dataset and getting reviews for the above 20 products
     Top20_Prod_reviews =    Item_Reviews[Item_Reviews['name'].isin(Top20_Recomm_products_df.index)]
-    Top20_Prod_reviews.insert(15, "Sentiscore", 0 )
-    for index, row in Top20_Prod_reviews.iterrows():
-        review_str = Top20_Prod_reviews.at[index,'reviews_text']
+    #Top20_Prod_reviews.insert(15, "Sentiscore", 0 )
+    #for index, row in Top20_Prod_reviews.iterrows():
+     #   review_str = Top20_Prod_reviews.at[index,'reviews_text']
         #Result=self.sentimental_Score(review_str)
-        test_inp = review_str.split('.')
-        Result = xgb_model_pk.predict(word_vectorizer.transform(test_inp))
-        Top20_Prod_reviews.at[index,'Sentiscore'] = Result[0]
+        #test_inp = review_str.split('.')
+        #Result = xgb_model_pk.predict(word_vectorizer.transform(test_inp))
+      #  Result = xgb_model_pk.predict(word_vectorizer.transform(review_str.split('.')))
+       # Top20_Prod_reviews.at[index,'Sentiscore'] = Result[0]
         #Top20_Prod_reviews.at[index,'Sentiscore'] = xgb_model_pk.predict(word_vectorizer.transform(test_inp))
-    Top5_recommended_products=Top20_Prod_reviews.groupby(['name']).sum().sort_values(by='Sentiscore',ascending=False)[0:5]
+    Top5_recommended_products=Top20_Prod_reviews.groupby(['name']).sum().sort_values(by='user_sentiment',ascending=False)[0:5]
 
     #output = "Predicted Iris Class: " + str(class_predicted)
     output = "TOP 5 Recommended Products are : " + str(Top5_recommended_products)
