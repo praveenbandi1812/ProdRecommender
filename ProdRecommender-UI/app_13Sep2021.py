c536c60cf3600cdcccfbb24d995278f9285bc391
praveenbandi1812 <86944280+praveenbandi1812@users.noreply.github.com>
2021-09-13 09:13:28 +0800

    13sep0913am

--- ProdRecommender-UI/app_13Sep2021.py
+++ ProdRecommender-UI/app_13Sep2021.py
@@ -10,37 +10,33 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 # Create Flask object to run
 app = Flask(__name__)
 
+xgb_model_pk = joblib.load('xgb_model.pkl')
 item_final_rating_pk = joblib.load('item_final_rating.pkl')
+Product_Reviews = joblib.load('Product_Reviews_CleanData.pkl')
+word_vectorizer = joblib.load('word_vectorizer.pkl')
 Item_Reviews = joblib.load('Item_Reviews.pkl')
-#xgb_model_pk = joblib.load('xgb_model.pkl')
-#Product_Reviews = joblib.load('Product_Reviews_CleanData.pkl')
-#word_vectorizer = joblib.load('word_vectorizer.pkl')
-
 
 @app.route('/')
 def home():
+    #return "Sentiment ier"
     return render_template('index.html')
 
 @app.route('/predict')
 def predict():
     user_input = request.args['username']
+    #Top20_Recomm_products=item_final_rating_pk.loc[user_input].sort_values(ascending=False)[0:20]
     try:
         Top20_Recomm_products_df = pd.DataFrame(item_final_rating_pk.loc[user_input].sort_values(ascending=False)[0:20])
         ##### merging with main dataset and getting reviews for the above 20 products
         Top20_Prod_reviews =    Item_Reviews[Item_Reviews['name'].isin(Top20_Recomm_products_df.index)]
-        ####### Following code measures sentiment using model, but it takes around 25 seconds, to avoid timeout, I used generated sentiment
-        #Top20_Prod_reviews.insert(15, "Sentiscore", 0 )
-        #for index, row in Top20_Prod_reviews.iterrows():
-        #    review_str = Top20_Prod_reviews.at[index,'reviews_text']
-        #    Result = self.xgb_model_pk.predict(self.word_vectorizer.transform(review_str.split('.')))
-        #    Top20_Prod_reviews.at[index,'Sentiscore'] = Result[0]
-        ##### End of on the fly sentiment code
         Top5_recommended_products=Top20_Prod_reviews.groupby(['name']).sum().sort_values(by='user_sentiment',ascending=False)[0:5]
         Top5_recommended_products_df = pd.DataFrame(Top5_recommended_products.index)
         output2 = Top5_recommended_products_df.to_string(header=False,index=False)
         output= output2.split("\n")
     except Exception as e:
         output = "Sorry, we are unable to predict for the given user"
+    #return render_template('index.html', header='TOP 5 Recommended Products', prediction_text='Recommended Products are - {}'.format(output))
     return render_template('index.html', header='TOP 5 Recommended Products', prediction_text=output)
+    #return render_template('index.html',header='Amazing Universe', sub_header='Our universe is quite amazing', list_header="Galaxies!",prediction_text=output, site_title="Camposha.info")
 if __name__ == "__main__":
     app.run()
